71bf93e4b45ce3b0a7b671b98ca71826a7caa093
huangyao618 <2227589842@qq.com>
2020-08-15 23:14:51 +0800

    revise a file

--- Week_01/合并两个有序数组.java
+++ Week_01/合并两个有序数组.java
@@ -1,38 +1,34 @@
 class Solution {
     /*
-    //借鉴的滚学球解法
-    public int removeDuplicates(int[] nums) {
-        if(nums==null||nums.length==0) return 0;
-        if(nums.length==1) return 1;
-        int len = nums.length;
-        int res = 0;
-        int delNum = 0;//记录当前项前面有多少个需要删除的重复项
-        for(int i=0;i<len;i++) {
-            if(i==0) {
-                res++;
-                continue;
-            }
-            if(nums[i]==nums[i-1]) {
-                delNum++;
-            }else {
-                nums[i-delNum] = nums[i];
-                res++;
+    //指针法从后往前遍历，利用num1的空出的空间
+    //时间复杂度O（n）,空间复杂度O（1）
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        if(nums2==null||nums2.length==0)  return;
+        int end1 = m-1, end2 = n-1,end = m+n-1;
+        while(end1>=0&&end2>=0){
+            if(nums1[end1]>nums2[end2]){
+                nums1[end--] = nums1[end1--];
+            }else{
+                 nums1[end--] = nums2[end2--];              
             }
         }
-        return res;
+        if(end1==-1){
+            while(end2>=0)   nums1[end--] = nums2[end2--];
+
+        }
     }
     */
-    public int removeDuplicates(int[] nums) {
-        //指针法，用一个指针记录每一个非重复项需要插入的索引位置
-        if(nums==null||nums.length==0) return 0;
-        if(nums.length==1) return 1;
-        int len = nums.length, res = 1,i=1;
-        for(int j=1;j<len;j++){
-            if(nums[j]!=nums[j-1]){
-                nums[i++] = nums[j];
-                res++; 
-            }
-        }
-        return res;
-    }
+    /*
+    //内部使用了归并排序，空间复杂度为O（（m+n）log(m+n)）;空间复杂度O(1)
+     public void merge(int[] nums1, int m, int[] nums2, int n) {
+         System.arraycopy(nums2,0,nums1,m,n);
+         //System.arraycopy(arr1,start1,arr2,start2,len)
+         //arr1 要被复制的数组
+         //start1 复制的起始位置
+         //arr2 目标数组
+         //m 被复制到目标数组的起始位置
+         //len 需要复制的长度
+         Arrays.sort(nums1);
+     }   
+     */
 }
